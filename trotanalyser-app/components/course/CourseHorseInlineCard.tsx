d924d95eb2507dd6b4672922772630108cbac8d8
Mat-gyver <mc26121979@gmail.com>
2026-03-11 21:34:50 +0100

    Update CourseHorseInlineCard.tsx

--- trotanalyser-app/components/course/CourseHorseInlineCard.tsx
+++ trotanalyser-app/components/course/CourseHorseInlineCard.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { memo } from "react";
 import { View } from "react-native";
 
 type Props = {
   children?: React.ReactNode;
 };
 
-export default function CourseHorseInlineCard({ children }: Props) {
-  return <View>{children}</View>;
+function CourseHorseInlineCard({ children }: Props) {
+  return (
+    <View
+      style={{
+        marginBottom: 8,
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: "rgba(255,255,255,0.03)",
+      }}
+    >
+      {children}
+    </View>
+  );
 }
+
+export default memo(CourseHorseInlineCard);
