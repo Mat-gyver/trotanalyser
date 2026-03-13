12b3a331894f07890afabe15e6ec00a2d415dce4
Mat-gyver <mc26121979@gmail.com>
2026-03-13 19:10:33 +0100

    Update index.tsx

--- trotanalyser-app/app/(tabs)/index.tsx
+++ trotanalyser-app/app/(tabs)/index.tsx
@@ -11,9 +11,7 @@ import {
     View,
 } from "react-native";
 
-const API_BASE =
-process.env.EXPO_PUBLIC_API_BASE ||
-window.location.origin.replace(/-\d+\.app\.github\.dev$/, "-8000.app.github.dev");
+import { API_BASE } from "../../constants/courseApiBase";
 
 type Course = {
     reunion: string;
