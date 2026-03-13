598490c10437cf6db2c4ea3a54be29dde8937ed8
Mat-gyver <mc26121979@gmail.com>
2026-03-13 18:49:39 +0100

    Update courseApiBase.ts

--- trotanalyser-app/constants/courseApiBase.ts
+++ trotanalyser-app/constants/courseApiBase.ts
@@ -1,3 +1,5 @@
 export const API_BASE =
-process.env.EXPO_PUBLIC_API_BASE ||
-window.location.origin.replace(/-\d+\.app\.github\.dev$/, "-8000.app.github.dev");
+  process.env.EXPO_PUBLIC_API_BASE ||
+  (typeof window !== "undefined"
+    ? window.location.origin.replace(/-\d+\.app\.github\.dev$/, "-8000.app.github.dev")
+    : "");
