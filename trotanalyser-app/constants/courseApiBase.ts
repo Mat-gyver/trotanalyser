997f724777134c3b193abcd9ebee24ccaf129c53
Mat-gyver <mc26121979@gmail.com>
2026-03-14 09:20:22 +0100

    Update API_BASE to use localhost URL
    Set API_BASE to a default localhost URL.

--- trotanalyser-app/constants/courseApiBase.ts
+++ trotanalyser-app/constants/courseApiBase.ts
@@ -1,12 +1,1 @@
-const fromEnv = (process.env.EXPO_PUBLIC_API_BASE || "").replace(/\/$/, "");
-
-export const API_BASE = fromEnv;
-
-export function assertApiBase() {
-  if (!API_BASE) {
-    throw new Error(
-      "EXPO_PUBLIC_API_BASE manquant. Configure l'URL publique du backend avant de lancer l'app."
-    );
-  }
-  return API_BASE;
-}
+export const API_BASE = "http://localhost:8000";
