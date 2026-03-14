490fd796e67b35ee00c9267ce53049c81e3b34e3
Mat-gyver <mc26121979@gmail.com>
2026-03-14 12:09:04 +0100

    Update API_BASE to use production or development base

--- trotanalyser-app/constants/courseApiBase.ts
+++ trotanalyser-app/constants/courseApiBase.ts
@@ -1,6 +1,8 @@
 import Constants from "expo-constants";
 
-const getApiBase = () => {
+const PROD_API_BASE = process.env.EXPO_PUBLIC_API_BASE?.trim();
+
+const getDevApiBase = () => {
   const debuggerHost = Constants.expoConfig?.hostUri;
 
   if (debuggerHost) {
@@ -11,4 +13,4 @@ const getApiBase = () => {
   return "http://localhost:8000";
 };
 
-export const API_BASE = getApiBase();
+export const API_BASE = PROD_API_BASE || getDevApiBase();
