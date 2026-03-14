148d28766278326cb7b740d522ac54e624330ead
Mat-gyver <mc26121979@gmail.com>
2026-03-14 23:49:19 +0100

    Update analysis.py

--- trotanalyser-app/backend/analysis.py
+++ trotanalyser-app/backend/analysis.py
@@ -1,1 +1,9 @@
+def analyse_forme(musique):
 
+    if not musique:
+        return "forme récente peu lisible"
+
+    if "1" in musique[:4]:
+        return "forme récente favorable"
+
+    return "forme récente correcte"
