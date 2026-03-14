25883cb5cfd5df37ea804777c11f67ff81a0eba4
Mat-gyver <mc26121979@gmail.com>
2026-03-14 17:35:58 +0100

    Update courseApiBase.ts

--- trotanalyser-app/constants/courseApiBase.ts
+++ trotanalyser-app/constants/courseApiBase.ts
@@ -1,16 +1,2 @@
-import Constants from "expo-constants";
-
-const PROD_API_BASE = process.env.EXPO_PUBLIC_API_BASE?.trim();
-
-const getDevApiBase = () => {
-  const debuggerHost = Constants.expoConfig?.hostUri;
-
-  if (debuggerHost) {
-    const host = debuggerHost.split(":").shift();
-    return `http://${host}:8000`;
-  }
-
-  return "http://localhost:8000";
-};
-
-export const API_BASE = PROD_API_BASE || getDevApiBase();
+export const API_BASE =
+  "https://animated-journey-g4g4q5xq9446h9gvq-8000.app.github.dev";
